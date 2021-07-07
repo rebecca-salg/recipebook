332edfe8ce855742ddbd99f83c8d14e1fd88cd17
Winglet80 <frederik@sonsky.se>
2021-07-07 14:14:01 +0200

    first commit

---
+++ src/main/java/se/group2/Recepies/RecepiesApplication.java
@@ -0,0 +1,13 @@
+package se.group2.Recepies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/group2/Recepies/RecepiesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.group2.Recepies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
