2de0e095c0e63bd7e649fe5b4f95a6f530d1c264
Pauline <pauline.j.andersson@gmail.com>
2021-07-08 14:29:44 +0200

    styling of startpage for logged in user

--- src/main/java/se/group2/Recepies/LoggedInStartController.java
+++ src/main/java/se/group2/Recepies/LoggedInStartController.java
@@ -1,13 +1,26 @@
 package se.group2.Recepies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class LoggedInStartController {
 
-    @GetMapping("/user/start")
-    String start(){
+    @Autowired
+    RecipeRepository repository;
+
+    @GetMapping("/user")
+    String start(Model model){
+        List<Recipe> recentRecipes = new ArrayList<>();
+        for(int i = repository.getRecipes().size()-1; i >= 0; i--){
+            recentRecipes.add(repository.getRecipes().get(i));
+        }
+        model.addAttribute("recipes", recentRecipes);
         return "loggedInStart";
     }
 }
