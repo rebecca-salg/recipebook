1f5c6ea49bdb62f3fb72e67c55c858c37f6f91d5
Pauline <pauline.j.andersson@gmail.com>
2021-07-08 15:40:52 +0200

    merge conflict

--- src/main/java/se/group2/Recepies/AddRecipeController.java
+++ src/main/java/se/group2/Recepies/AddRecipeController.java
@@ -3,7 +3,9 @@ package se.group2.Recepies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,23 +17,28 @@ public class AddRecipeController {
     RecipeRepository repository;
 
     @GetMapping("/add")
-    String add() {
+    String add(){
         return "addRecipeView";
     }
 
     @PostMapping("/add")
-    String addRecipe(Model recipe, @RequestParam String title,
-                   @RequestParam String description,
-                     @RequestParam Category category) {
-
-
+    String addRecipe(Model recipe, @RequestParam String title, @RequestParam String description, @RequestParam Category category,
+                     @RequestParam("names") List<String> names, @RequestParam("units") String[] units, @RequestParam("amount") int[] amount){
         Recipe recipe1 = new Recipe();
+        List<Ingredient> ingredientList = new ArrayList<>();
+
+        for(int i=0; i<names.size(); i++){
+            ingredientList.add(new Ingredient(names.get(i), units[i], amount[i]));
+        }
+
+        recipe1.setIngredients(ingredientList);
         recipe1.setTitle(title);
         recipe1.setId(repository.getRecipes().size()+1);
         recipe1.setDescription(description);
         recipe1.setCategory(category);
         repository.addRecipe(recipe1);
-        recipe.addAttribute("repository", repository);
+        recipe1.setId(repository.getRecipes().size()+1);
+        recipe.addAttribute("recipes", repository);
         return "addRecipeView";
     }
 }
