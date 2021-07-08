7b99bd623bb7c5ad43e60a567c6364dfa9d977e2
Pauline <pauline.j.andersson@gmail.com>
2021-07-08 14:50:57 +0200

    added check for logged in or not and redirect

--- src/main/java/se/group2/Recepies/LoggedInStartController.java
+++ src/main/java/se/group2/Recepies/LoggedInStartController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,12 +19,17 @@ public class LoggedInStartController {
     RecipeRepository repository;
 
     @GetMapping("/user")
-    String start(Model model){
+    String start(HttpSession session, Model model){
         List<Recipe> recentRecipes = new ArrayList<>();
-        for(int i = repository.getRecipes().size()-1; i >= 0; i--){
-            recentRecipes.add(repository.getRecipes().get(i));
+        if(session.getAttribute("username") == "admin@hotmail.com"){
+            for(int i = repository.getRecipes().size()-1; i >= 0; i--){
+                recentRecipes.add(repository.getRecipes().get(i));
+            }
+            model.addAttribute("recipes", recentRecipes);
+            return "loggedInStart";
+        } else {
+            return "redirect:/login";
         }
-        model.addAttribute("recipes", recentRecipes);
-        return "loggedInStart";
+
     }
 }
