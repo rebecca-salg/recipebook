13955e742099a61ca7d2aacb784e5449d7422a53
Winglet80 <frederik@sonsky.se>
2021-07-08 10:16:06 +0200

    Fixed recipes and recipe and added contructor to repository

--- src/main/java/se/group2/Recepies/RecipeRepository.java
+++ src/main/java/se/group2/Recepies/RecipeRepository.java
@@ -10,6 +10,39 @@ public class RecipeRepository {
 
     public List<Recipe> recipes = new ArrayList<>();
 
+    public RecipeRepository() {
+        // TODO remove generic recipe creations
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipe.setTitle("Spaghetti med paprika och salladsost");
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(new Ingredient("Salt", "kryddmått", 1.0));
+        recipe.setIngredients(ingredients);
+        recipe.setDescription("Hacka lite. Blanda lite.\n\nKoka upp vattnet och lägg i pastan.");
+        recipe.setCategory(Category.CATEGORY_MAIN);
+        addRecipe(recipe);
+
+        recipe = new Recipe();
+        recipe.setId(2L);
+        recipe.setTitle("Påskkyckling med äppelcider");
+        ingredients = new ArrayList<>();
+        ingredients.add(new Ingredient("Peppar", "kryddmått", 2.0));
+        recipe.setIngredients(ingredients);
+        recipe.setDescription("Dränk kycklingen.");
+        recipe.setCategory(Category.CATEGORY_MAIN);
+        addRecipe(recipe);
+
+        recipe = new Recipe();
+        recipe.setId(3L);
+        recipe.setTitle("Rabarberpaj med sås");
+        ingredients = new ArrayList<>();
+        ingredients.add(new Ingredient("Rabarber", "styck", 4.0));
+        recipe.setIngredients(ingredients);
+        recipe.setDescription("Kör in i ugnen");
+        recipe.setCategory(Category.CATEGORY_DESSERT);
+        addRecipe(recipe);
+    }
+
     public void addRecipe(Recipe recipe) {
         recipes.add(recipe);
     }
