56d4f8a3b1bf4a34327de27922c027a46ee852f1
Pauline <pauline.j.andersson@gmail.com>
2021-07-08 15:26:11 +0200

    trying to fix css

--- src/main/java/se/group2/Recepies/LoginController.java
+++ src/main/java/se/group2/Recepies/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 
@@ -14,12 +16,14 @@ public class LoginController {
         return "login";
     }
     @PostMapping("/login")
-    public String loginPageInfo(@RequestParam(required = false,defaultValue = " ") String username,
+    public String loginPageInfo(HttpSession session,  @RequestParam(required = false,defaultValue = " ") String username,
                                 @RequestParam(required = false,defaultValue = " ") String password){
         if (username.equals("admin@hotmail.com") && password.equals("123")){
-            return "";
+            session.setAttribute("username", username);
+            return "redirect:/user";
+        } else {
+            return "login";
         }
-        return "login";
     }
 
     @GetMapping("/register")
@@ -44,13 +48,6 @@ public class LoginController {
         return "index";
     }
 
-    @PostMapping("/")
-    public String startPageInfo(@RequestParam(required = false,defaultValue = " ") String username, @RequestParam(required = false,defaultValue = " ") String password){
-        if (username.equals("admin@hotmail.com") && password.equals("123")){
-            return "";
-        }
-        return "redirect:/user";
-    }
 
     @GetMapping("/profile")
     public String profilePage(){
