d3de962e91d0a2671a9a0cd2f96d558517457db0
Winglet80 <frederik@sonsky.se>
2021-07-08 14:30:51 +0200

    Menu fixes

--- src/main/java/se/group2/Recepies/LoginController.java
+++ src/main/java/se/group2/Recepies/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 
@@ -13,11 +15,15 @@ public class LoginController {
     public String loginPage(){
         return "login";
     }
+
     @PostMapping("/login")
-    public String loginPageInfo(@RequestParam String username, @RequestParam String password){
+    public String loginPageInfo(HttpSession session, @RequestParam String username, @RequestParam String password){
         if (username.equals("admin") && password.equals("123")){
-            return "";
+            session.setAttribute("username", username);
+
+            return "redirect:/user";
         }
+
         return "login";
     }
 }
