228d743261f1d386f3c0b41263f5de8af4c4bc56
Pauline <pauline.j.andersson@gmail.com>
2021-08-05 12:59:41 +0200

    validation when editing

--- src/main/java/se/group2/Recepies/LoginController.java
+++ src/main/java/se/group2/Recepies/LoginController.java
@@ -4,12 +4,14 @@ package se.group2.Recepies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -89,9 +91,13 @@ public class LoginController {
     }
 
     @PostMapping("/edit")
-    public String userData(@ModelAttribute User user, HttpSession session) {
-
+    public String userData(Model model, @Valid User user, BindingResult result, HttpSession session) {
         User currentUser = (User)session.getAttribute("user");
+        if(result.hasErrors()){
+            model.addAttribute("edit", true);
+            return "registrationEdit";
+        }
+
         user.setId(currentUser.getId());
         userRepository.save(user);
 
