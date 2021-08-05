9943460f07d9ff6c5925d28c0b3ef24017d36466
Pauline <pauline.j.andersson@gmail.com>
2021-08-05 15:12:04 +0200

    .

--- src/main/java/se/group2/Recepies/LoginController.java
+++ src/main/java/se/group2/Recepies/LoginController.java
@@ -1,6 +1,5 @@
 package se.group2.Recepies;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
