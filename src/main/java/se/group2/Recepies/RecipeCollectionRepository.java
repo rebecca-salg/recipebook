f6525cd15a0ee70836daa21408784cf2f1bd5bab
Andreas Valtersson <Andreas_valter@hotmail.com>
2021-08-05 10:27:20 +0200

    Updating branch

--- src/main/java/se/group2/Recepies/RecipeCollectionRepository.java
+++ src/main/java/se/group2/Recepies/RecipeCollectionRepository.java
@@ -2,5 +2,9 @@ package se.group2.Recepies;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RecipeCollectionRepository extends CrudRepository<RecipeCollection, Long> {
+
+    List<User> findAllRecipeByUserId(Long id);
 }
