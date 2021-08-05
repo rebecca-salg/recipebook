5a8205eacff547b718834b518b1092525694f956
Andreas Valtersson <Andreas_valter@hotmail.com>
2021-08-05 15:12:19 +0200

    Memberpage

--- src/main/java/se/group2/Recepies/UserRepository.java
+++ src/main/java/se/group2/Recepies/UserRepository.java
@@ -2,7 +2,10 @@ package se.group2.Recepies;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
 
 
     Iterable<User> findByUsername(String username);
